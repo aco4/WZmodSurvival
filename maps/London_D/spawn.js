13e330c40e6ad11d4753b6fd6815ff77214822b7
aco4 <anthonythegu@gmail.com>
2026-04-15 07:18:47 -0400

    London rework

--- maps/London_D/spawn.js
+++ maps/London_D/spawn.js
@@ -2,17 +2,32 @@ namespace("spawn_")
 
 function spawn_eventMissionTimeout()
 {
-	spawn_tick();
+	queue("spawn_tick");
 }
 
 function spawn_tick()
 {
-	if (countDroid(DROID_ANY, ENEMY) < 1500)
-	{
-		const [x, y] = SPAWN_POINTS[syncRandom(SPAWN_POINTS.length)];
-		const template = TEMPLATES[syncRandom(TEMPLATES.length)];
-		Template.spawn(Template.fromString(template), ENEMY, x, y);
+	const minute = Math.ceil(gameTime / 1000 / 60);
+	const length = Math.min(TEMPLATES.length, minute);
+	const template = TEMPLATES[syncRandom(length)];
+
+	if (template) {
+		const [x, y] = spawn_positions[syncRandom(spawn_positions.length)];
+		addDroid(ENEMY, x, y, template.name, template.body, template.propulsion, "", "", ...template.turrets);
 	}
-	const delay = 30000 * Math.E**(-0.000001*gameTime);
-	queue("spawn_tick", delay);
+
+	queue("spawn_tick");
 }
+
+const spawn_positions = [
+	[5, 1],
+	[6, 1],
+	[7, 1],
+	[8, 1],
+	[9, 1],
+	[10, 1],
+	[11, 1],
+	[12, 1],
+	[13, 1],
+	[62, 8],
+];
