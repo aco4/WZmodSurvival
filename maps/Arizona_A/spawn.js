32b46050234040557e77c8f04e6f5fc4f93bc53f
aco4 <anthonythegu@gmail.com>
2026-04-22 22:23:40 -0400

    Add Arizona

---
+++ maps/Arizona_A/spawn.js
@@ -0,0 +1,18 @@
+namespace("spawn_")
+
+function spawn_eventMissionTimeout()
+{
+	queue("spawn_tick");
+}
+
+function spawn_tick()
+{
+	const minute = Math.ceil(gameTime / 1000 / 60);
+	const length = Math.min(TEMPLATES.length, minute);
+	const template = TEMPLATES[syncRandom(length)];
+	if (template)
+	{
+		addDroid(ENEMY, 16, 33, template.name, template.body, template.propulsion, "", "", ...template.turrets);
+	}
+	queue("spawn_tick", 1000);
+}
