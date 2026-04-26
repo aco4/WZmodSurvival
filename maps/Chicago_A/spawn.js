e3616bef8ac7a131ea058cf73ad4c6268cb105ec
aco4 <anthonythegu@gmail.com>
2026-04-26 16:51:38 -0400

    Re-add Chicago: use default templates

---
+++ maps/Chicago_A/spawn.js
@@ -0,0 +1,59 @@
+namespace("spawn_")
+
+function spawn_eventMissionTimeout()
+{
+	queue("spawn_tick");
+}
+
+function spawn_tick()
+{
+	if (spawn_positions.length === 0)
+	{
+		return;
+	}
+	const minute = Math.ceil(gameTime / 1000 / 60);
+	const length = Math.min(TEMPLATES.length, minute);
+	const template = TEMPLATES[syncRandom(length)];
+	if (template)
+	{
+		const [x, y] = spawn_positions[syncRandom(spawn_positions.length)];
+		hackNetOff();
+		addDroid(ENEMY, x, y, template.name, template.body, template.propulsion, "", "", ...template.turrets);
+		hackNetOn();
+	}
+	queue("spawn_tick", spawn_delay);
+}
+
+const spawn_delay = (() =>
+{
+	switch (playerData[ENEMY].difficulty)
+	{
+		case INSANE: return 100;
+		case HARD  : return 200;
+		case MEDIUM: return 300;
+		case EASY  : return 400;
+		default    : return 500;
+	}
+})();
+
+const spawn_positions = [
+	[22, 1],
+	[23, 1],
+	[24, 1],
+	[25, 1],
+	[26, 1],
+	[27, 1],
+	[28, 1],
+	[29, 1],
+	[30, 1],
+	[31, 1],
+	[32, 1],
+	[33, 1],
+	[34, 1],
+	[35, 1],
+	[36, 1],
+	[37, 1],
+	[38, 1],
+	[39, 1],
+	[62, 126]
+];
